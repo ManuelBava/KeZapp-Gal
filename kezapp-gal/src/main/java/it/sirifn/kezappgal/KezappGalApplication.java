fc4aabd92028a2382395425a793d5cb66c6eb1c8
leoIapp <leoiappelli01@gmail.com>
2021-03-01 11:49:49 +0100

    spr iniz

---
+++ kezapp-gal/src/main/java/it/sirifn/kezappgal/KezappGalApplication.java
@@ -0,0 +1,13 @@
+package it.sirifn.kezappgal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KezappGalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KezappGalApplication.class, args);
+	}
+
+}
---
+++ kezapp-gal/src/test/java/it/sirifn/kezappgal/KezappGalApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirifn.kezappgal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KezappGalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
