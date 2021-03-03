341904485c30f3795f3887dfd5083e5c77441d3c
ManuelBava <manuelbava1996@gmail.com>
2021-03-03 09:21:27 +0100

    Update KezappGalApplication.java

--- kezapp-Gal-server/src/main/java/it/sirifn/kezappgal/KezappGalApplication.java
+++ kezapp-Gal-server/src/main/java/it/sirifn/kezappgal/KezappGalApplication.java
@@ -1,7 +1,16 @@
 package it.sirifn.kezappgal;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"it.sirfin.kezappgal"})
+@EnableJpaRepositories(basePackages = {"it.sirfin.kezappgal.repository"})
+@EntityScan(basePackages = {"it.sirfin.kezappgal.model"})
 public class KezappGalApplication {
 
     public static void main(String[] args) {
