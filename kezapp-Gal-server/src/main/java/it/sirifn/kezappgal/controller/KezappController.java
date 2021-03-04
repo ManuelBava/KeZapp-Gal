2b25718cb982cfb3666db904a7018cf24ba07e98
leoIapp <leoiappelli01@gmail.com>
2021-03-04 10:27:33 +0100

    levato commenti

--- kezapp-Gal-server/src/main/java/it/sirifn/kezappgal/controller/KezappController.java
+++ kezapp-Gal-server/src/main/java/it/sirifn/kezappgal/controller/KezappController.java
@@ -1,47 +1,47 @@
-//package it.sirifn.kezappgal.controller;
-//
-//import it.sirifn.kezappgal.dto.InviaMessaggioDto;
-//import it.sirifn.kezappgal.dto.RegistrazioneDto;
-//import it.sirifn.kezappgal.dto.RichiediMessaggioDto;
-//import it.sirifn.kezappgal.dto.RichiediRegistrazioneDto;
-//import it.sirifn.kezappgal.service.KezappService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@CrossOrigin("*")
-//@RestController
-//public class KezappController {
-//
-//    @Autowired
-//    KezappService kezappService;
-//
-//    @RequestMapping("/registrazione")
-//    @ResponseBody
-//    public RegistrazioneDto registrazione(@RequestBody RichiediRegistrazioneDto rrdto){
-//        
-//        throw new UnsupportedOperationException("Not supported yet."); 
-//    }
-//    
-//    @RequestMapping("/invio-a-tutti")
-//    @ResponseBody
-//    public InviaMessaggioDto inviaATutti(@RequestBody InviaMessaggioDto iatdto){
-//            throw new UnsupportedOperationException("Not supported yet."); 
-//
-//    }
-//   
-//    @RequestMapping("/invio-a-uno")
-//    @ResponseBody
-//   public InviaMessaggioDto inviaAUno(@RequestBody InviaMessaggioDto iaudto){
-//       throw new UnsupportedOperationException("Not supported yet."); 
-//   }
-//   
-//   @RequestMapping("/aggiorna")
-//    @ResponseBody
-//   public RichiediMessaggioDto aggiorna(@RequestBody RichiediMessaggioDto adto){
-//       throw new UnsupportedOperationException("Not supported yet."); 
-//   }
-//}
+package it.sirifn.kezappgal.controller;
+
+import it.sirifn.kezappgal.dto.InviaMessaggioDto;
+import it.sirifn.kezappgal.dto.RegistrazioneDto;
+import it.sirifn.kezappgal.dto.RichiediMessaggioDto;
+import it.sirifn.kezappgal.dto.RichiediRegistrazioneDto;
+import it.sirifn.kezappgal.service.KezappService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+public class KezappController {
+
+    @Autowired
+    KezappService kezappService;
+
+    @RequestMapping("/registrazione")
+    @ResponseBody
+    public RegistrazioneDto registrazione(@RequestBody RichiediRegistrazioneDto rrdto){
+        
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+    @RequestMapping("/invio-a-tutti")
+    @ResponseBody
+    public InviaMessaggioDto inviaATutti(@RequestBody InviaMessaggioDto iatdto){
+            throw new UnsupportedOperationException("Not supported yet."); 
+
+    }
+   
+    @RequestMapping("/invio-a-uno")
+    @ResponseBody
+   public InviaMessaggioDto inviaAUno(@RequestBody InviaMessaggioDto iaudto){
+       throw new UnsupportedOperationException("Not supported yet."); 
+   }
+   
+   @RequestMapping("/aggiorna")
+    @ResponseBody
+   public RichiediMessaggioDto aggiorna(@RequestBody RichiediMessaggioDto adto){
+       throw new UnsupportedOperationException("Not supported yet."); 
+   }
+}
