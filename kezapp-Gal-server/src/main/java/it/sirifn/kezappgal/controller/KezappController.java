d0ea424d6f9a5682c8c60a36ae9e53d8048c1369
ManuelBava <manuelbava1996@gmail.com>
2021-03-04 11:28:32 +0100

    .

--- kezapp-Gal-server/src/main/java/it/sirifn/kezappgal/controller/KezappController.java
+++ kezapp-Gal-server/src/main/java/it/sirifn/kezappgal/controller/KezappController.java
@@ -21,27 +21,28 @@ public class KezappController {
 
     @RequestMapping("/registrazione")
     @ResponseBody
-    public RegistrazioneDto registrazione(@RequestBody RichiediRegistrazioneDto rrdto){
-        
-        throw new UnsupportedOperationException("Not supported yet."); 
+    public RegistrazioneDto registrazione(@RequestBody RichiediRegistrazioneDto rrdto) {
+        RegistrazioneDto reg = new RegistrazioneDto();
+        reg = kezappService.registrazione(rrdto);
+        return reg;
     }
-    
+
     @RequestMapping("/invio-a-tutti")
     @ResponseBody
-    public InviaMessaggioDto inviaATutti(@RequestBody InviaMessaggioDto iatdto){
-            throw new UnsupportedOperationException("Not supported yet."); 
+    public InviaMessaggioDto inviaATutti(@RequestBody InviaMessaggioDto iatdto) {
+        throw new UnsupportedOperationException("Not supported yet.");
 
     }
-   
+
     @RequestMapping("/invio-a-uno")
     @ResponseBody
-   public InviaMessaggioDto inviaAUno(@RequestBody InviaMessaggioDto iaudto){
-       throw new UnsupportedOperationException("Not supported yet."); 
-   }
-   
-   @RequestMapping("/aggiorna")
+    public InviaMessaggioDto inviaAUno(@RequestBody InviaMessaggioDto iaudto) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @RequestMapping("/aggiorna")
     @ResponseBody
-   public RichiediMessaggioDto aggiorna(@RequestBody RichiediMessaggioDto adto){
-       throw new UnsupportedOperationException("Not supported yet."); 
-   }
+    public RichiediMessaggioDto aggiorna(@RequestBody RichiediMessaggioDto adto) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
